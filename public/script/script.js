dcb01030e29da91810db320d8cbee99885a02fb9
codewithkevin <oseikelvin709@gmail.com>
2022-04-29 17:20:52 +0000

    creating dark mode

--- public/script/script.js
+++ public/script/script.js
@@ -8,56 +8,11 @@ const dark = document.querySelector(".dark-mode");
 
 //Event Listeners 
 button.addEventListener("click", chatbot);
+dark.addEventListener("click", darkmode);
 
 
 
-//Logic Functions
-
-// function displayJoke(){    
-    
-//     const baseURL = "https://v2.jokeapi.dev/joke/";
-//     const categories = ["Programming", "Misc", "Pun"];
-//     const params = [
-//         "blacklistFlags=nsfw,religious,racist",
-//         "idRange=0-100"
-//     ];
-//     const url = baseURL + categories[Math.floor(Math.random() * categories.length)] + "?" + params[Math.floor(Math.random() * params.length)];
-    
-
-//     var xhr = new XMLHttpRequest();
-//     xhr.open("GET", url, true);
-
-
-
-//     xhr.onreadystatechange = function() {
-//         if(xhr.readyState == 4 && xhr.status < 300) // readyState 4 means request has finished + we only want to parse the joke if the request was successful (status code lower than 300)
-//         {
-//             var randomJoke = JSON.parse(xhr.responseText);
-    
-//             if(randomJoke.type == "single")
-//             {
-//                 // If type == "single", the joke only has the "joke" property
-//                 par.innerHTML = randomJoke.joke;
-//             }
-//             else
-//             {
-//                 // If type == "single", the joke only has the "joke" property
-//                 par.innerHTML = randomJoke.setup + "<br>" + randomJoke.delivery;
-//                 // par.innerHTML += randomJoke.delivery;
-//             }
-//         }
-//         else if(xhr.readyState == 4)
-//         {
-//             alert("Error while requesting joke.\n\nStatus code: " + xhr.status + "\nServer response: " + xhr.responseText);
-//         }
-//     };
-    
-//     xhr.send();
-// } 
-
-
-//Function
-
+//Function to display the joke
 function chatbot(event) {
     event.preventDefault();
 
@@ -127,3 +82,7 @@ function chatbot(event) {
     list.appendChild(chatdiv)
 }
 
+//Function to Change the Dark Mode
+function darkmode() {
+    
+}
