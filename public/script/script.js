d782ef91c94a01453c93b700268acaec6643b2ab
codewithkevin <oseikelvin709@gmail.com>
2022-04-29 12:25:07 +0000

    update

--- public/script/script.js
+++ public/script/script.js
@@ -9,9 +9,104 @@ button.addEventListener("click", chatbot);
 
 
 
+//Logic Functions
+// function displayJoke(){    
+    
+//     const baseURL = "https://v2.jokeapi.dev/joke/";
+//     const categories = ["Programming", "Misc", "Pun"];
+//     const params = [
+//         "blacklistFlags=nsfw,religious,racist",
+//         "idRange=0-100"
+//     ];
+//     const url = baseURL + categories[Math.floor(Math.random() * categories.length)] + "?" + params[Math.floor(Math.random() * params.length)];
+    
+
+//     var xhr = new XMLHttpRequest();
+//     xhr.open("GET", url, true);
+
+
+
+//     xhr.onreadystatechange = function() {
+//         if(xhr.readyState == 4 && xhr.status < 300) // readyState 4 means request has finished + we only want to parse the joke if the request was successful (status code lower than 300)
+//         {
+//             var randomJoke = JSON.parse(xhr.responseText);
+    
+//             if(randomJoke.type == "single")
+//             {
+//                 // If type == "single", the joke only has the "joke" property
+//                 par.innerHTML = randomJoke.joke;
+//             }
+//             else
+//             {
+//                 // If type == "single", the joke only has the "joke" property
+//                 par.innerHTML = randomJoke.setup + "<br>" + randomJoke.delivery;
+//                 // par.innerHTML += randomJoke.delivery;
+//             }
+//         }
+//         else if(xhr.readyState == 4)
+//         {
+//             alert("Error while requesting joke.\n\nStatus code: " + xhr.status + "\nServer response: " + xhr.responseText);
+//         }
+//     };
+    
+//     xhr.send();
+// } 
+
+
 //Function
 function chatbot(event) {
     event.preventDefault();
+
+    //Computer Logic 
+    const baseURL = "https://v2.jokeapi.dev/joke/";
+    const categories = ["Programming", "Misc", "Pun"];
+    const params = [
+        "blacklistFlags=nsfw,religious,racist",
+        "idRange=0-100"
+    ];
+    const url = baseURL + categories[Math.floor(Math.random() * categories.length)] + "?" + params[Math.floor(Math.random() * params.length)];
+    
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+
+
+
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState == 4 && xhr.status < 300) // readyState 4 means request has finished + we only want to parse the joke if the request was successful (status code lower than 300)
+        {
+            var randomJoke = JSON.parse(xhr.responseText);
+    
+            if(randomJoke.type == "single")
+            {
+                // If type == "single", the joke only has the "joke" property
+
+                //Computer Reply
+                    const newreply = document.createElement("li");
+                    newreply.innerHTML = randomJoke.joke;
+                    newreply.classList.add("chat-item")
+                    chatdiv.appendChild(newreply);
+                 
+            }
+            else
+            {
+                // If type == "single", the joke only has the "joke" property
+                //  randomJoke.setup + "<br>" + randomJoke.delivery;
+                // par.innerHTML += randomJoke.delivery;
+                //Computer Reply
+                const newreply = document.createElement("li");
+                newreply.innerHTML = randomJoke.setup + "<br>" + randomJoke.delivery;
+                newreply.classList.add("chat-item")
+                chatdiv.appendChild(newreply);
+            }
+        }
+        else if(xhr.readyState == 4)
+        {
+            alert("Error while requesting joke.\n\nStatus code: " + xhr.status + "\nServer response: " + xhr.responseText);
+        }
+    };
+    
+    xhr.send();
 
     //Chat Div 
     const chatdiv = document.createElement("div");
@@ -23,14 +118,8 @@ function chatbot(event) {
     newchat.classList.add("chat-item");
     chatdiv.appendChild(newchat);
 
-    //Computer Reply
-    const newreply = document.createElement("li");
-    newreply.innerHTML = ("HEllo World");
-    newreply.classList.add("chat-item")
-    chatdiv.appendChild(newreply);
 
     //Append List 
     list.appendChild(chatdiv)
 }
 
-//Logic Functions 
