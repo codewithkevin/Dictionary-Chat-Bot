3df60be2ea1b3f00e3a0b6d6f60de5f6ec2c0cf8
codewithkevin <oseikelvin709@gmail.com>
2022-04-29 12:05:16 +0000

    update

---
+++ public/script/script.js
@@ -0,0 +1,36 @@
+//Selectors
+const input = document.querySelector(".bot-input")
+const button = document.querySelector(".chat-button")
+const list = document.querySelector(".chatbox-list")
+const reply = document.querySelector(".computer-reply")
+
+//Event Listeners 
+
+button.addEventListener("click", chatbot);
+
+
+
+//User Function
+function chatbot(event) {
+    event.preventDefault();
+
+    //Chat Div 
+    const chatdiv = document.createElement("div");
+    chatdiv.classList.add("chat");
+
+    //Create Li 
+    const newchat = document.createElement("li");
+    newchat.innerHTML = input.value;
+    newchat.classList.add("chat-item");
+    chatdiv.appendChild(newchat);
+
+    //Computer Reply
+    const newreply = document.createElement("li");
+    newreply.innerHTML = ("HEllo World");
+    newreply.classList.add("chat-item")
+    chatdiv.appendChild(newreply);
+
+    //Append List 
+    list.appendChild(chatdiv)
+}
+
