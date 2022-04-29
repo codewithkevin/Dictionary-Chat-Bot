59dff00869bafcba6de925bca900acfce83bcfaf
codewithkevin <oseikelvin709@gmail.com>
2022-04-29 10:41:11 +0000

    update

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/public', express.static('public'));
+
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
 
 
 app.listen('3000', function() {
