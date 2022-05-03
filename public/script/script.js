df3bf080ebe0e66e25ec9267663da2362e4fc43a
codewithkevin <oseikelvin709@gmail.com>
2022-05-03 19:21:09 +0000

    update

--- public/script/script.js
+++ public/script/script.js
@@ -4,68 +4,32 @@
 const input = document.querySelector(".bot-input")
 const button = document.querySelector(".chat-button")
 const list = document.querySelector(".chatbox-list")
-const reply = document.querySelector(".computer-reply")
 const displayrule = document.querySelector(".box-bundle");
 
 //Event Listeners 
 button.addEventListener("click", chatbot);
 
 
+//Functions
 
-//Function Game logic
-function game(){
-    
+function chatbot(event) {
+
     //Remove Rule 
     displayrule.remove();
 
-    //Computer Logic 
-    const baseURL = "https://v2.jokeapi.dev/joke/";
-    const categories = ["Programming", "Misc", "Pun"];
-    const params = [
-        "blacklistFlags=nsfw,religious,racist",
-        "idRange=0-100"
-    ];
-    const url = baseURL + categories[Math.floor(Math.random() * categories.length)] + "?" + params[Math.floor(Math.random() * params.length)];
+    event.preventDefault();
+
+    //Get API
+    const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
     
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-
-
-
-    xhr.onreadystatechange = function() {
-        if(xhr.readyState == 4 && xhr.status < 300) // readyState 4 means request has finished + we only want to parse the joke if the request was successful (status code lower than 300)
-        {
-            var randomJoke = JSON.parse(xhr.responseText);
+    async function getword(){
+    const response = await fetch(url + input.value);
+    const data = await response.json();
+    const {meanings} = data[0];
+    const meaningofword = meanings[0].definitions[0].definition;
     
-            if(randomJoke.type == "single")
-            {
-                // If type == "single", the joke only has the "joke" property
-
-                //Computer Reply
-                    const newreply = document.createElement("li");
-                    newreply.innerHTML = randomJoke.joke;
-                    newreply.classList.add("reply-item")
-                    chatdiv.appendChild(newreply);
-                 
-            }
-            else
-            {
-                //Computer Reply
-                const newreply = document.createElement("li");
-                newreply.innerHTML = randomJoke.setup + "<br>" + randomJoke.delivery;
-                newreply.classList.add("reply-item")
-                chatdiv.appendChild(newreply);
-            }
-        }
-        else if(xhr.readyState == 4)
-        {
-            alert("Error while requesting joke.\n\nStatus code: " + xhr.status + "\nServer response: " + xhr.responseText);
-        }
-    };
     
-    xhr.send();
-
     //Chat Div 
     const chatdiv = document.createElement("div");
     chatdiv.classList.add("chat");
@@ -76,41 +40,22 @@ function game(){
     newchat.classList.add("chat-item");
     chatdiv.appendChild(newchat);
 
+    //Computer Reply
+    const newreply = document.createElement("li");
+    newreply.innerHTML = meaningofword;
+    newreply.classList.add("reply-item")
+    chatdiv.appendChild(newreply);
+
+
 
     //Append List 
     list.appendChild(chatdiv)
-
+    
     return;
 }
-
-//Function to display the joke
-
-const start = ['joke', 'next', 'lol']
-
-
-function chatbot(event) {
-        
-    event.preventDefault();
-    
-    // else {
-    //     //Computer Reply
-    //     const newreply = document.createElement("li");
-    //     newreply.innerHTML = "I don't understand what you mean.";
-    //     newreply.classList.add("reply-item")
-    //     chatdiv.appendChild(newreply);
-    // }
-    for (const item in start){
-        if (input.value.toLowerCase() === start[item]) {
-            game();
-            return;
-        }
-    }
+    getword();
     
 }
-
-
-
-
 
 //Function to Change the Dark Mode
 const chk = document.getElementById('chk');
